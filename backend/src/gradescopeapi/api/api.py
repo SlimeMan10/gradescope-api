fa1397f14bbc48f2e2c7f9ebdae1b55d87c2541d
SlimeMan10 <justinhernlin@icloud.com>
2025-04-25 19:30:02 -0700

    fixed gradescope link

--- backend/src/gradescopeapi/api/api.py
+++ backend/src/gradescopeapi/api/api.py
@@ -28,7 +28,7 @@ API_PREFIX = "/api"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://gradescope-api.vercel.app",  # Production frontend
+        "https://gradescope-assignment.vercel.app/",  # Production frontend
         "http://localhost:3000",              # Local development
         "http://localhost:5173"               # Vite default port
     ],
